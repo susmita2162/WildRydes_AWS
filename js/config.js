11de33209345ccef24a676f29ac580c765b2035f
Susmita Madineni <susmitamadineni@gmail.com>
2024-01-31 13:25:11 -0500

    Uploading frontend code

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_517kzKEXr', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '66g3o6eacnj8p304mm572n19qn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://gh1nc4mf3d.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
